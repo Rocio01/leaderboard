b1e1dca62027adf553191b2a77959ea5b8e59cae
Zulma Rocio <zrmartinezg@gmail.com>
2021-10-07 11:49:37 -0500

    Refactor display function

---
+++ src/api.js
@@ -0,0 +1,36 @@
+const gameId = 'ZDxIEZDwVgeQEcdrzL0c';
+const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`;
+
+const postScore = async (name, score) => {
+  const params = {};
+  params.user = name.value;
+  params.score = score.value;
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(params),
+  });
+  const message = await response.json();
+  if (message.message === 'Error') {
+    throw new Error(message.message);
+  }
+  console.log(message);
+  return message;
+};
+
+const getScores = async () => {
+  try {
+    const response = await fetch(url);
+
+    const data = await response.json();
+
+    const { result } = data;
+
+    display(result);
+    console.log(result);
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+export{postScore, getScores}
